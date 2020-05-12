9bf187752c2f20ce2e05fd579efbf4377422337b
JosephW <22502454+memoryaccess@users.noreply.github.com>
2020-05-12 19:20:05 +0100

    Added mongo connection and schemas through mongodb 🌿

--- src/modules/events/messageReactionAdd.js
+++ src/modules/events/messageReactionAdd.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const Discord = require('discord.js') // eslint-disable-line no-unused-vars
 
 /**
@@ -5,8 +6,7 @@ const Discord = require('discord.js') // eslint-disable-line no-unused-vars
  * @param {Discord.MessageReaction} messageReaction message reaction
  * @param {Discord.User} user user that triggered reaction
  */
+
 module.exports = (client, messageReaction, user) => {
-
-
 
 }
