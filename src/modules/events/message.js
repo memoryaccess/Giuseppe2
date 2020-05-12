d9931368684b92b2f361ff0b3a65781492d31dce
JosephW <22502454+memoryaccess@users.noreply.github.com>
2020-05-12 23:42:40 +0100

    Added basic swore filter 🤬🤐🙊

--- src/modules/events/message.js
+++ src/modules/events/message.js
@@ -2,15 +2,28 @@ const Discord = require('discord.js') // eslint-disable-line no-unused-vars
 // const mongo = require('../../mongo/connect')
 const integrityCheck = require('../../integrityCheck')
 
+const badWords = ['braed', 'no hugs', 'exploit', 'crack', 'nigger', 'faggot', 'nigga', 'fag ', 'synapse', 'nig ', 'discord.gg/', 'discord.com/invite/', 'discordapp.com/invite/', 'phantom', 'ph4ntom']
+const sworeFilter = new RegExp(badWords.join('|'), 'gi')
+
+const hasBadWord = (message) => {
+    //TODO: Better permissions system configurable per-server.
+    if (message.member.hasPermission('KICK_MEMBERS')) return false
+    return sworeFilter.test(message.content)
+}
+
 /**
  * @param {Discord.Client} client bot client
  * @param {Discord.Message} message received message
  */
 module.exports = (client, message) => {
 
-    if (message.author.bot) return
     if (!integrityCheck.checkMessageIntegrity(message)) return
+    if (message.author.bot) return
 
+    if(hasBadWord(message)){
+        //TODO: Log the message the deleted/edited channel, but replace the swores with [deleted]
+        return message.delete()
+    }
 
     // commands below :^))))
 
